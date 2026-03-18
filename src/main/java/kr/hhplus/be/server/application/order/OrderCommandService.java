f1fc6a24c60047cd468dc0f06a4af467b017095e
Kimsangwon <doncham98@gmail.com>
2026-03-18 10:22:25 +0900

    refactor: 주문 로직 리팩토링
    메서드 분리

--- src/main/java/kr/hhplus/be/server/application/order/OrderCommandService.java
+++ src/main/java/kr/hhplus/be/server/application/order/OrderCommandService.java
@@ -7,99 +7,95 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import kr.hhplus.be.server.application.point.PointPort;
 import kr.hhplus.be.server.domain.address.Address;
 import kr.hhplus.be.server.domain.cartItem.CartItem;
-import kr.hhplus.be.server.application.cartItem.CartItemPort;
 import kr.hhplus.be.server.domain.inventoryReserve.InventoryReservation;
 import kr.hhplus.be.server.domain.order.Order;
 import kr.hhplus.be.server.domain.point.Point;
 import kr.hhplus.be.server.domain.point.exception.PointNotEnoughException;
+import kr.hhplus.be.server.domain.point.exception.PointNotFoundException;
 import kr.hhplus.be.server.domain.point.exception.PointUseNegativeException;
 import kr.hhplus.be.server.domain.pointReservation.PointReservation;
 import kr.hhplus.be.server.exception.ErrorCode;
 import kr.hhplus.be.server.domain.inventory.Inventory;
 import kr.hhplus.be.server.domain.inventory.exception.InSufficientStockException;
 import kr.hhplus.be.server.domain.inventory.exception.NotFoundInventoryException;
-import kr.hhplus.be.server.application.address.AddressPort;
-import kr.hhplus.be.server.application.inventory.InventoryPort;
 import kr.hhplus.be.server.domain.order.ShippingInfo;
 import kr.hhplus.be.server.api.order.request.OrderDraftCreateRequest;
 import kr.hhplus.be.server.api.order.response.OrderDraftCreateResponse;
 import kr.hhplus.be.server.domain.orderproduct.OrderProduct;
 import kr.hhplus.be.server.domain.user.User;
-import kr.hhplus.be.server.domain.user.UserPort;
+import kr.hhplus.be.server.infrastructure.persistence.address.AddressRepository;
+import kr.hhplus.be.server.infrastructure.persistence.cartItem.CartItemRepository;
+import kr.hhplus.be.server.infrastructure.persistence.inventory.InventoryRepository;
 import kr.hhplus.be.server.infrastructure.persistence.inventoryReserve.InventoryReserveRepository;
+import kr.hhplus.be.server.infrastructure.persistence.order.OrderRepository;
+import kr.hhplus.be.server.infrastructure.persistence.point.PointRepository;
 import kr.hhplus.be.server.infrastructure.persistence.pointReservation.PointReservationRepository;
+import kr.hhplus.be.server.infrastructure.persistence.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class OrderCommandService implements OrderUseCase {
-	private final OrderPort orderPort;
-	private final AddressPort addressPort;
-	private final UserPort userPort;
-	private final CartItemPort cartItemPort;
-	private final InventoryPort inventoryPort;
-	private final PointPort pointPort;
+	private final OrderRepository orderRepo;
+	private final AddressRepository addressRepo;
+	private final UserRepository userRepo;
+	private final PointRepository pointRepo;
+	private final CartItemRepository cartItemRepo;
+	private final InventoryRepository inventoryRepo;
+
 	private final InventoryReserveRepository inventoryReserveRepo;
 	private final PointReservationRepository pointReservationRepo;
 
 	@Transactional
 	@Override
 	public OrderDraftCreateResponse createOrder(OrderDraftCreateRequest request) {
-		Long addressId = request.getAddressId();
-		String memo = request.getMemo();
-		Long cartId = request.getCartId();
 		// JWT 대체 예정
-		Long userId = request.getUserId();
-		Long couponId = request.getCouponId();
-		String idempotencyKey = request.getIdempotencyKey();
 
-		Address address = addressPort.loadAddress(addressId);
-		ShippingInfo shippingInfo = new ShippingInfo(address);
+		ShippingInfo shippingInfo = createShippingInfo(request.getAddressId());
 
-		User user = userPort.loadUser(userId);
-		Order draft = Order.createDraft(user, shippingInfo, idempotencyKey);
-		// 동시 요청 시 유니크 예외 터지면서 OrderFacade에서 조회를 통해 정상 응답과 같은 응답을 해줌.
-		Order orderDraft = orderPort.saveAndFlush(draft);
+		Order orderDraft = createOrderDraft(request.getUserId(), shippingInfo, request.getIdempotencyKey());
 
 		// 사용할 포인트 검증
-		Long pointUseAmount = request.getPointUseAmount();
-		if(pointUseAmount == null) pointUseAmount = 0L;
-		if(pointUseAmount < 0)
-			throw new PointUseNegativeException(ErrorCode.NEGATIVE_POINT_USE, orderDraft.getId());
+		Long pointUseAmount = normalizePointUseAmount(request.getPointUseAmount(), orderDraft.getId());
 
-		// point 조회 및 검증
-		// 락을 통해 동시성 관리: 포인트 충전, 포인트 사용, 만료 로직에서 임계영역인 balance를 보호함.
-		Point point = pointPort.findByUserIdForUpdate(userId);
-		// 포인트 부족 예외
-		if(point.availablePoint() < pointUseAmount) {
-			throw new PointNotEnoughException(ErrorCode.NOT_ENOUGH_POINT, point.getId());
-		}
-		// point 예약 + order에 point 관련 필드 넣기
-		point.reservePoint(pointUseAmount);
-		pointReservationRepo.save(
-			PointReservation.reserve(orderDraft.getId(), userId, pointUseAmount)
-		);
+		// point 조회, 잔액 검증 및 예약
+		// 락을 통해 동시성 관리: 포인트 충전, 포인트 사용, 만료 로직에서 임계영역인 point.balance를 보호함.
+		reservePoint(request.getUserId(), pointUseAmount, orderDraft);
 
 		// cart 조회할 때 product도 한번에 fetch 조인으로 가져오자
-		List<CartItem> cartItems = cartItemPort.findByCartIdWithProduct(cartId);
+		List<CartItem> cartItems = cartItemRepo.findByCartIdWithProduct(request.getCartId());
 
 		// productId를 뽑기
-		List<Long> productsIds = cartItems.stream()
-			.map(cartItem -> cartItem.getProduct().getId())
-			.collect(Collectors.toList());
-		// 데드락 방지를 위해 정렬 후 락을 건다.
-		productsIds.sort(Long::compareTo);
+		List<Long> productsIds = extractSortedProductIds(cartItems);
 
-		//List<Inventory> inventories = inventoryPort.findByProductIdInOrderByProductId(productsIds);
-		List<Inventory> inventories = inventoryPort.findByProductIdInForUpdateOrderByProductId(productsIds);
-		// <productId, Inventory> 맵 생성
-		Map<Long, Inventory> inventoryMap = inventories.stream()
-			.collect(Collectors.toMap(inv -> inv.getProduct().getId(), inv -> inv));
+		// 재고 조회 -> <productId, Inventory> 맵 생성
+		//List<Inventory> inventories = inventoryPort.findByProductIdInForUpdateOrderByProductId(productsIds);
+		Map<Long, Inventory> inventoryMap = lockInventoriesByProductId(productsIds);
 
-		// 재고 검증 + reserved 증가
+		// 재고 검증 + 예약
+		reserveInventories(cartItems, inventoryMap, orderDraft);
+
+		// cartItem -> orderProduct 변환
+		List<OrderProduct> orderProducts = createOrderProducts(cartItems);
+
+		orderDraft.completeOrderDraft(orderProducts, request.getCouponId(), 0L, request.getMemo(), pointUseAmount);
+		return OrderDraftCreateResponse.from(orderDraft);
+	}
+
+	private static List<OrderProduct> createOrderProducts(List<CartItem> cartItems) {
+		List<OrderProduct> orderProducts = cartItems.stream().map(ci ->
+			OrderProduct.create(ci.getProduct().getId(),
+				ci.getProduct().getName(),
+				ci.getProduct().getPrice(),
+				ci.getQty()
+			)
+		).collect(Collectors.toList());
+		return orderProducts;
+	}
+
+	private void reserveInventories(List<CartItem> cartItems, Map<Long, Inventory> inventoryMap, Order orderDraft) {
 		for(CartItem ci : cartItems) {
 			// cartItem.getProduct().getId()로 inventory 조회 -> 수량 확인 -> 재고 부족 시 예외 처리
 			Inventory inv = inventoryMap.get(ci.getProduct().getId());
@@ -114,16 +110,58 @@ public class OrderCommandService implements OrderUseCase {
 			inventoryReserveRepo.save(invReserve);
 			inv.reserveStock(ci.getQty());
 		}
-		// cartItem -> orderProduct 변환
-		List<OrderProduct> orderProducts = cartItems.stream().map(ci ->
-			OrderProduct.create(ci.getProduct().getId(),
-				ci.getProduct().getName(),
-				ci.getProduct().getPrice(),
-				ci.getQty()
-			)
-		).collect(Collectors.toList());
+	}
 
-		orderDraft.completeOrderDraft(orderProducts, couponId, 0L, memo, pointUseAmount);
-		return OrderDraftCreateResponse.from(orderDraft);
+	private Map<Long, Inventory> lockInventoriesByProductId(List<Long> productsIds) {
+		List<Inventory> inventories = inventoryRepo.findByProductIdInForUpdate(productsIds);
+		Map<Long, Inventory> inventoryMap = inventories.stream()
+			.collect(Collectors.toMap(inv -> inv.getProduct().getId(), inv -> inv));
+		return inventoryMap;
+	}
+
+	private static List<Long> extractSortedProductIds(List<CartItem> cartItems) {
+		List<Long> productsIds = cartItems.stream()
+			.map(cartItem -> cartItem.getProduct().getId())
+			.collect(Collectors.toList());
+		// 데드락 방지를 위해 정렬 후 락을 건다.
+		productsIds.sort(Long::compareTo);
+		return productsIds;
+	}
+
+	private void reservePoint(Long userId, Long pointUseAmount, Order orderDraft) {
+		Point point = pointRepo.findByUserIdForUpdate(userId)
+			.orElseThrow(() -> new PointNotFoundException(ErrorCode.NOT_FOUND_POINT, userId));
+		// 포인트 부족 예외
+		if(point.availablePoint() < pointUseAmount) {
+			throw new PointNotEnoughException(ErrorCode.NOT_ENOUGH_POINT, point.getId());
+		}
+
+		point.reservePoint(pointUseAmount);
+		pointReservationRepo.save(
+			PointReservation.reserve(orderDraft.getId(), userId, pointUseAmount)
+		);
+	}
+
+	private Order createOrderDraft(Long userId, ShippingInfo shippingInfo, String idempotencyKey) {
+		User user = userRepo.findById(userId)
+			.orElseThrow(() -> new IllegalArgumentException("Invalid user ID"));
+		Order draft = Order.createDraft(user, shippingInfo, idempotencyKey);
+		// 동시 요청 시 유니크 예외 터지면서 OrderFacade에서 조회를 통해 정상 응답과 같은 응답을 해줌.
+		Order orderDraft = orderRepo.saveAndFlush(draft);
+		return orderDraft;
+	}
+
+	private static Long normalizePointUseAmount(Long pointUseAmount, Long orderDraftId) {
+		if(pointUseAmount == null) pointUseAmount = 0L;
+		if(pointUseAmount < 0)
+			throw new PointUseNegativeException(ErrorCode.NEGATIVE_POINT_USE, orderDraftId);
+		return pointUseAmount;
+	}
+
+	private ShippingInfo createShippingInfo(Long addressId) {
+		Address address = addressRepo.findById(addressId)
+			.orElseThrow(() -> new IllegalArgumentException("Invalid address ID"));
+		ShippingInfo shippingInfo = new ShippingInfo(address);
+		return shippingInfo;
 	}
 }
